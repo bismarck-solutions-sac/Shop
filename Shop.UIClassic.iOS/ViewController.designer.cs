84f154ef6885d6803813c354bd3006caf55e9a9a
Jose Arroyo Chavez <jose.arroyo@bismarck.com.pe>
2020-04-10 15:42:39 -0500

    iOS

---
+++ Shop.UIClassic.iOS/ViewController.designer.cs
@@ -0,0 +1,51 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace Shop.UIClassic.iOS
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIActivityIndicatorView ActivityIndicator { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField EmailText { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField PasswordText { get; set; }
+
+        [Action ("UIButton342_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void UIButton342_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (ActivityIndicator != null) {
+                ActivityIndicator.Dispose ();
+                ActivityIndicator = null;
+            }
+
+            if (EmailText != null) {
+                EmailText.Dispose ();
+                EmailText = null;
+            }
+
+            if (PasswordText != null) {
+                PasswordText.Dispose ();
+                PasswordText = null;
+            }
+        }
+    }
+}
